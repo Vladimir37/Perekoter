1064cac57687dc6c2f973202cc110f92246324fa
Vladimir37 <vladimir_zapas@mail.ru>
2016-09-26 23:23:25 +0000

    Basis 0.4

--- Perekoter.go
+++ Perekoter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Perekoter/models"
 	"fmt"
+	"os"
 	//"github.com/gin-gonic/gin"
 	//"github.com/aubm/interval"
 )
@@ -10,6 +11,7 @@ import (
 func main() {
 	models.Init()
 	fmt.Println("Connection to the database was created!")
+	os.Mkdir("./covers", 0777)
 	// router := gin.Default()
 
 	// router.Run(":8080")
--- controllers/threads.go
+++ controllers/threads.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"strconv"
+    "io"
+    "os"
+    "time"
 
 	"Perekoter/models"
 
@@ -22,4 +25,37 @@ func AddThread(c *gin.Context) {
 
 	var targetBoard models.Board
 	db.First(&targetBoard, boardNum)
+
+    imageName := strconv.FormatInt(time.Now().Unix(), 10) + ".png"
+
+    img, _, errImg := c.Request.FormFile("cover")
+    out, errFile := os.Open("./covers/" + imageName)
+
+    if (errFile != nil) || (errImg != nil) {
+        c.JSON(200, gin.H{
+            "status": 1,
+        })
+    }
+
+    defer out.Close()
+
+    _, errWriting := io.Copy(out, img)
+
+    if errWriting != nil {
+        c.JSON(200, gin.H{
+            "status": 2,
+        })
+    }
+
+
+    db.Create(&models.Thread{
+        Numbering: numbering,
+        Roman: roman,
+        CurrentNum: currentNum,
+        Title: title,
+        HeaderLink: headerLink,
+        Header: header,
+        Image: imageName,
+        Board: targetBoard,
+    })
 }
